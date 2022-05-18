1438f3cd1bd18cc2d2524b47f8ed89d318ce4920
Voityk Yurii <maxaon.otto@gmail.com>
2022-05-18 17:32:42 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,69 @@
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://aeworld.com/wp-content/uploads/2021/12/CELINE_WOMEN_SUMMER22_PRINT_10.jpg",
+        title: 'Spring Sale!',
+        description: "Don't miss Your Chanse! Save up to 50% on every purchase!",
+        bg: '819351',
+    },
+    {
+        id: 2,
+        img: "https://1l1qtl3ivlys18bq884b04ci-wpengine.netdna-ssl.com/wp-content/gallery/summer-collection-2021/1943_QP_E-COMMERCE_WEBSITE_SUMMER-2021_FLUID-BOOK.jpg",
+        title: 'Summer Sale!',
+        description: "Don't miss Your Chanse! Save up to 30% on every purchase!",
+        bg: 'e5d6cd',
+    },
+    {
+        id: 3,
+        img: "https://habituallychic.luxury/wp-content/uploads/2019/09/sezane-sutumn-september-2019-habituallychic-003-730x1024.jpg",
+        title: 'Autumn Sale!',
+        description: "Don't miss Your Chanse! Save up to 40% on every purchase!",
+        bg: '977a67',
+    }
+];
+
+export const categories = [
+    {
+        "id": 1,
+        "img": "https://www.schiesser.com/out/pictures/generated/product/3/760_1100_90/american-t-shirts-rundhals-2er-pack-weiss-essentials-208150-100-detail1.jpg",
+        "title": "SHIRTS"
+    },
+    {
+        "id": 1,
+        "img": "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa/global/845855/04/fnd/UKR/w/1000/h/1000/fmt/png",
+        "title": "LONGEWEAR"
+    },
+    {
+        "id": 1,
+        "img": "https://5.imimg.com/data5/NM/LX/MY-42532489/mens-black-jacket-500x500.jpg",
+        "title": "JACKETS"
+    }
+];
+
+export const popularProducts = [
+    {
+        "id": 1,
+        "img": "https://cdn-ssl.s7.disneystore.com/is/image/DisneyShopping/5622045537882?fmt=jpeg&qlt=90&wid=652&hei=652",
+    },
+    {
+        "id": 2,
+        "img": "https://12ax7web.s3.amazonaws.com/accounts/1/products/1986199879943/Ramen-Panda-tahiti-blue-light-t-shirt-teeturtle-full-21-1000x1000.jpg",
+    },
+    {
+        "id": 3,
+        "img": "https://therealfashionista.com/wp-content/uploads/2020/05/jaime-shrayber-topshop-green-matching-loungewear-set-5-scaled.jpeg",
+    },
+    {
+        "id": 4,
+        "img": "https://cdn11.bigcommerce.com/s-6gf5gg/images/stencil/500x659/products/24066/69143/Ulla-Long-Sleeve-Top-Sandstone-by-Natori__17443.1.jpg",
+    },
+    {
+        "id": 5,
+        "img": "https://www.neweracap.co.uk/globalassets/products/b5193_282/13102981/new-york-yankees-mlb-patch-navy-varsity-jacket-13102981-center.jpg?w=400",
+    },
+    {
+        "id": 6,
+        "img": "https://5.imimg.com/data5/BV/UY/ZP/SELLER-59756470/mens-cotton-jacket-500x500.jpg",
+    }
+]
